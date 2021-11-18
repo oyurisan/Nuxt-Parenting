c61c2a59d6f1b953d6bb4b2deb335142569afb32
abechihiro <abe.1.0.2.3.mssp@gmail.com>
2021-11-18 10:18:04 +0900

    Nav追加！

---
+++ components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="Nav-main">
+    <div>Hellow World!!!</div>
+    <div>Navピースだよ!!!</div>
+
+</div>
+</template>
+
+<style>
+.Nav-main {
+    width: 100%;
+    background-color: darkgrey;
+    text-align: center;
+    padding: 5%;
+}
+
+</style>
