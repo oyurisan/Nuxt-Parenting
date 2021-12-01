27131182a3e852a400a197c7985c2cee0c6785c2
sakamotoyuri <oyurisann68@gmail.com>
2021-12-01 11:11:53 +0900

    微修正

--- pages/growth.vue
+++ pages/growth.vue
@@ -24,16 +24,13 @@
 {{item.m}}ヶ月
         </option>
       </select>
-    <div class="m-3">
-    <button class="px-2 py-1 bg-red-700 text-xl text-white font-semibold rounded hover:bg-red-800 w-40" @click="addweight">
-      <!-- <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+ <div class="m-3">
+    <button class="px-2 py-1 bg-red-900 text-xl text-white font-semibold rounded hover:bg-red-900 w-56" @click="addheight">
+      <div class="button">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="30px" height="30px">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
-</svg> -->
-身長を登録する</button>
-</div>
-   <svg xmlns="http://www.w3.org/2000/svg" className="h-1 w-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="20px" height="20px">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
-</svg>
+</svg>&nbsp;&nbsp;&nbsp;
+身長を登録する</div></button></div>
       <div>体重</div>
          <input v-model="weightdate" type="date" name="Date" /><br>
       <select v-model="weightk">
@@ -56,7 +53,13 @@
 {{item.m}}ヶ月
         </option>
       </select>
-    <button class="touroku" @click="addweight">体重を登録する</button>
+     <div class="m-3">
+    <button class="px-2 py-1 bg-red-900 text-xl text-white font-semibold rounded hover:bg-red-900 w-56" @click="addweight">
+      <div class="button">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="30px" height="30px">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+</svg>&nbsp;&nbsp;&nbsp;
+体重を登録する</div></button></div>
     {{weightg}}
     {{weightk}}{{weightyear}}{{weightmonth}}
    
@@ -154,5 +157,8 @@ export default {
   text-align: center;
   font-size: 200%;
   margin-top: 10%;
+}
+.button{
+  display: flex;
 }
 </style>
